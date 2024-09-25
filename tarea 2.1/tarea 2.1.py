23f7a46dceae854dccaa1a9ff9161a6867171d9b
April25-hub <a.23308051281173@cbtis128.edu.mx>
2024-09-24 20:09:20 -0600

    Add files via upload

---
+++ tarea 2.1/tarea 2.1.py
@@ -0,0 +1,23 @@
+
+#Arzaba Diaz April 
+
+#esta linea indica mi nombre 
+
+#Mi practica tratara de crear un codigo  donde solicite tres numeros y se realice la multiplicacion de ellos
+
+#esta linea indica la indicacion del programa
+
+num1 = float(input("ingresar ingresar el primer numero:"))
+
+num2 = float(input("ingresar ingresar el segundo numero:"))
+
+num3 = float(input("ingresar ingresar el tercer numero:"))
+
+#realizar la multiplicacion d elos tres numeros
+
+resultado = num1*num2*num3
+
+
+#mostrar el resultado de la multiplicacion
+
+print("el resultado de la multiplicacion es:",resultado)
